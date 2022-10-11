36c4ba962ea2f47d459453ebb8712b5aa87f6018
fire83274 <fire83274@gmail.com>
2022-10-11 10:49:23 +0300

    Create header and footer

---
+++ Assets/JS/tail.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        colors: {            
+            'figma-dgray': '#10242E',
+            'figma-gray': '#CBCBCB',
+            'figma-lgray': '#F0EFF3',
+            'figma-yellow': '#FFD800',
+            'figma-black':'#000000',            
+          },
+    },
+  };
