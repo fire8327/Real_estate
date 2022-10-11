3003a191b742385dd6c2491bd6a338d61a6566d4
fire83274 <fire83274@gmail.com>
2022-10-12 00:02:38 +0300

    create hot

---
+++ Assets/JS/slider.js
@@ -0,0 +1,30 @@
+/*jshint esnext: true */
+let defaultTransform = 0;
+        function goNext() {
+            defaultTransform = defaultTransform - 398;
+            var slider = document.getElementById("slider");
+            if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+            slider.style.transform = "translateX(" + defaultTransform + "px)";
+        }
+        next.addEventListener("click", goNext);
+        function goPrev() {
+            var slider = document.getElementById("slider");
+            if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+            else defaultTransform = defaultTransform + 398;
+            slider.style.transform = "translateX(" + defaultTransform + "px)";
+        }
+        prev.addEventListener("click", goPrev);
+        function goNext2() {
+            defaultTransform = defaultTransform - 398;
+            var slider2 = document.getElementById("slider2");
+            if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+            slider2.style.transform = "translateX(" + defaultTransform + "px)";
+        }
+        next2.addEventListener("click", goNext2);
+        function goPrev2() {
+            var slider2 = document.getElementById("slider2");
+            if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+            else defaultTransform = defaultTransform + 398;
+            slider2.style.transform = "translateX(" + defaultTransform + "px)";
+        }
+        prev2.addEventListener("click", goPrev2);
--- Assets/JS/tail.js
+++ Assets/JS/tail.js
@@ -10,6 +10,14 @@ tailwind.config = {
             'figma-bg':'#F5F5F5',    
             'figma-line':'#10242E',        
           },
+          screens: {
+            'ml':'425px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+          },
     },
   };
 
